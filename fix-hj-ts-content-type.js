a3eaf7b6022db0c1e18a3f907af9f99a1a7e61fb
m4a1dada <106305326+m4a1dada@users.noreply.github.com>
2026-08-08 20:31:35 +0800

    修复: Content-Type匹配改为indexOf以兼容charset后缀

--- fix-hj-ts-content-type.js
+++ fix-hj-ts-content-type.js
@@ -1,8 +1,8 @@
 // 淇娴疯瑙嗛TS鍒嗙墖Content-Type
-// iOS AVPlayer瑕佹眰TS鏂囦欢Content-Type涓簐ideo/mp2t锛屼絾鏈嶅姟鍣ㄨ繑鍥瀟ext/vnd.trolltech.linguist瀵艰嚧榛戝睆
+// 鏈嶅姟鍣ㄨ繑鍥瀟ext/vnd.trolltech.linguist; charset=utf-8锛宨OS AVPlayer鏃犳硶璇嗗埆瀵艰嚧榛戝睆
 if ($response && $response.headers) {
-    if ($response.headers['Content-Type'] === 'text/vnd.trolltech.linguist' ||
-        $response.headers['content-type'] === 'text/vnd.trolltech.linguist') {
+    var ct = $response.headers['Content-Type'] || $response.headers['content-type'] || '';
+    if (ct.indexOf('text/vnd.trolltech.linguist') !== -1) {
         $response.headers['Content-Type'] = 'video/mp2t';
         delete $response.headers['content-type'];
     }
